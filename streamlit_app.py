e5dc0804764618041b48d5af62e7e88075d1d24b
crkuchlenz <crkuchlenz@gmail.com>
2023-05-09 12:59:07 -0500

    Update streamlit_app.py
    removed arbitrary output

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 # get fruityvice api
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # writes the raw data to the screen
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
