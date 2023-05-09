b08287cc5b1d672082d27c705329e681c27b5b4c
crkuchlenz <crkuchlenz@gmail.com>
2023-05-09 12:54:57 -0500

    Update streamlit_app.py
    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
-strealit.header('Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 # get fruityvice api
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
